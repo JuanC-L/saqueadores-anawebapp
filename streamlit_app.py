b5b188bb4ef2cd8c41b9e50c530201b28f17a8c5
JuanC-L <jc.lindom@gmail.com>
2024-06-27 09:10:04 -0500

    ayuda2

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,8 +117,8 @@ st.sidebar.markdown("<h1 class='sidebar-title' style='text-align:center;'>Los Sa
 with st.sidebar:
     selected = option_menu(
         menu_title=None,
-        options=["Home", "Busqueda de Personas", "Dashboards 1", "Dashboards 2", "Dashboards 3"],
-        icons=["house", "person-fill", "coin", "book", "person-fill"],
+        options=["Home", "Busqueda de Personas", "Dashboard", "Apriori", "Random Forest","Clusters","ARIMA"],
+        icons=["house", "person-fill", "coin", "book", "person-fill", "book", "person-fill"],
         menu_icon="cast",
         default_index=0,
     )
@@ -212,7 +212,7 @@ if selected == 'Busqueda de Personas':
             else:
                 st.warning('No se encontraron resultados para el nombre ingresado.')
 
-if selected == "Dashboards 1":
+if selected == "Dashboard":
     total_appeared = df['aparecidos'].sum()
     total_disappeared = len(df) - df['aparecidos'].sum()
     percentage_appeared = (total_appeared / total_disappeared) * 100 if total_disappeared != 0 else 0
@@ -296,172 +296,73 @@ if selected == "Dashboards 1":
             fig_time = px.line(df_time, x='Fecha de Denuncia', y='Número de Personas', title='Personas Desaparecidas a lo Largo del Tiempo', color_discrete_sequence=['#AB63FA'])
             st.plotly_chart(fig_time)
             
-
-    # Footer
-    st.markdown("<hr style='border: 1px solid #FF6F61;'>", unsafe_allow_html=True)
-    st.markdown("<h4 style='text-align: center; color: #FF6F61;'>© 2024 Los Saqueadores</h4>", unsafe_allow_html=True)
-
-
-if selected == "Dashboards 2":
-    df['fecha_denuncia'] = pd.to_datetime(df['fecha_denuncia'], errors='coerce')
-    def convert_time_format(time_str):
-        if pd.isna(time_str):
-            return None
-        try:
-            return pd.to_datetime(time_str, format='%I:%M:%S %p').time()
-        except ValueError:
-            try:
-                return pd.to_datetime(time_str, format='%H:%M:%S').time()
-            except ValueError:
-                return None
-
-    df['hora_denuncia'] = df['hora_denuncia'].apply(convert_time_format)
-
-    total_disappeared = len(df)
-    total_appeared = df['aparecido'].sum()
-    percentage_appeared = (total_appeared / total_disappeared) * 100
-    region_most_disappeared = df['pais_nacimiento'].value_counts().idxmax()
-
-    df['Month'] = df['fecha_denuncia'].dt.month
-    df['Hour'] = df['fecha_denuncia'].dt.hour
-    df['Day'] = df['fecha_denuncia'].dt.day_name()
-
-    st.title('Dashboard de Personas Desaparecidas')
-
-    top_n = st.slider('Selecciona el número de dependencias policiales a mostrar:', min_value=1, max_value=20, value=10)
-
-    col1, space1, col2 = st.columns([4, 1, 4])
-
-    with col1:
-        st.subheader('Distribución de Edad')
-        fig_age = px.histogram(df, x='edad', nbins=20, title='Distribución de Edad', color_discrete_sequence=['#636EFA'])
-        st.plotly_chart(fig_age)
-
-    with col2:
-        st.subheader('Número de Personas Desaparecidas por País de Nacimiento')
-        country_counts = df['pais_nacimiento'].value_counts().reset_index()
-        country_counts.columns = ['País de Nacimiento', 'Número de Personas']
-        fig_country = px.pie(country_counts, names='País de Nacimiento', values='Número de Personas', 
-                            title='Número de Personas Desaparecidas por País de Nacimiento', 
-                            color_discrete_sequence=px.colors.qualitative.Pastel,
-                            hole=0.4)
-        fig_country.update_traces(textposition='inside', textinfo='percent+label')
-        st.plotly_chart(fig_country)
-
-    col3, space2, col4 = st.columns([4, 1, 4])
-
-    with col3:
-        st.subheader('Personas Desaparecidas a lo Largo del Tiempo')
-        df_time = df['fecha_denuncia'].dt.to_period('M').value_counts().sort_index().reset_index()
-        df_time.columns = ['Fecha de Denuncia', 'Número de Personas']
-        df_time['Fecha de Denuncia'] = df_time['Fecha de Denuncia'].astype(str)
-        fig_time = px.line(df_time, x='Fecha de Denuncia', y='Número de Personas', title='Personas Desaparecidas a lo Largo del Tiempo', color_discrete_sequence=['#AB63FA'])
-        st.plotly_chart(fig_time)
-
     st.header('Desapariciones por Horas y Días')
-    selected_month = st.selectbox('Selecciona un Mes:', sorted(df['Month'].dropna().unique().tolist()))
 
-    filtered_df = df[df['Month'] == selected_month]
+    # Supongamos que tu DataFrame se llama df y ya tiene las columnas 'fecha_hecho' y 'hora_hecho'
+    # Convierte las columnas 'fecha_hecho' y 'hora_hecho' a formato datetime
+    df['fecha_hecho'] = pd.to_datetime(df['fecha_hecho'], format='%d/%m/%Y')
+    df['hora_hecho'] = pd.to_datetime(df['hora_hecho'], format='%H:%M:%S').dt.time
 
-    heatmap_data = filtered_df.pivot_table(index='Day', columns='Hour', aggfunc='size', fill_value=0)
+    # Combina 'fecha_hecho' y 'hora_hecho' en una sola columna datetime
+    df['datetime_hecho'] = df.apply(lambda row: pd.to_datetime(f"{row['fecha_hecho'].date()} {row['hora_hecho']}"), axis=1)
 
-    fig_heatmap = px.imshow(heatmap_data, 
+    # Extrae el día de la semana y la hora
+    df['day_of_week'] = df['fecha_hecho'].dt.day_name()
+    df['hour'] = df['datetime_hecho'].dt.hour
+
+
+    min_date = df['fecha_hecho'].min()
+    max_date = df['fecha_hecho'].max()
+
+    selected_week = st.date_input("Selecciona una semana", [min_date, min_date + pd.Timedelta(days=6)])
+    start_date, end_date = selected_week[0], selected_week[1]
+
+    # Convertir start_date y end_date a datetime
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+
+    filtered_df = df[(df['fecha_hecho'] >= start_date) & (df['fecha_hecho'] <= end_date)]
+
+    # Pivot table para el heatmap
+    heatmap_data = filtered_df.pivot_table(index='day_of_week', columns='hour', aggfunc='size', fill_value=0)
+
+    # Ordenar los días de la semana
+    ordered_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    heatmap_data = heatmap_data.reindex(ordered_days)
+
+    fig_heatmap = px.imshow(heatmap_data,
                             labels=dict(x="Hora", y="Día de la Semana", color="Número de Desaparecidos"),
-                            x=heatmap_data.columns, 
+                            x=heatmap_data.columns,
                             y=heatmap_data.index,
                             color_continuous_scale='Viridis',
-                            title=f'Desapariciones en el Mes {selected_month}')
+                            title=f'Desapariciones del {start_date.strftime("%d/%m/%Y")} al {end_date.strftime("%d/%m/%Y")}')
+    fig_heatmap.update_layout(
+        autosize=False,
+        width=1200,  # Ajusta el ancho
+        height=600,  # Ajusta la altura
+        margin=dict(l=50, r=50, b=50, t=50, pad=4)
+    )
+
 
     st.plotly_chart(fig_heatmap)
-if selected == "Dashboards 3":
-    total_appeared = df['aparecidos'].sum()
-    total_disappeared = len(df) - df['aparecidos'].sum()
-    percentage_appeared = (total_appeared / total_disappeared) * 100 if total_disappeared != 0 else 0
-    region_most_disappeared = df['region'].value_counts().idxmax()
-
-    st.title('Dashboard de Personas Desaparecidas')
-    
-    # Use containers for better layout control
-    header_container = st.container()
-    metrics_container = st.container()
-    charts_container = st.container()
-
-    with header_container:
-        st.markdown("<hr style='border: 1px solid #FF6F61;'>", unsafe_allow_html=True)
-
-    with metrics_container:
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.metric(label="Cantidad de Desaparecidos", value=total_disappeared)
-        with col2:
-            st.metric(label="Cantidad de Aparecidos", value=total_appeared)
-        with col3:
-            st.metric(label="Región con más Desaparecidos", value=region_most_disappeared)
-
-    with charts_container:
-        st.markdown("<h2 style='color: #FF6F61;'>Visualizaciones</h2>", unsafe_allow_html=True)
-        st.markdown("<hr style='border: 1px solid #FF6F61;'>", unsafe_allow_html=True)
-        
-        col1, col2 = st.columns(2)
-
-        with col1:
-            fig_age = px.histogram(df, x='edad', nbins=20, title='Distribución de Edad', color_discrete_sequence=['#00CC96'])
-            age_counts = df['edad'].value_counts().reset_index()
-            age_counts.columns = ['Edad', 'Count']
-            max_age = age_counts.iloc[age_counts['Count'].idxmax()]['Edad']
-            max_count = age_counts['Count'].max()
-            min_age = age_counts.iloc[age_counts['Count'].idxmin()]['Edad']
-            min_count = age_counts['Count'].min()
-            mean_count = age_counts['Count'].mean()
-
-            fig_age.add_annotation(x=max_age, y=max_count,
-                                   text=f'Edad: {max_age}<br>Max: {max_count}',
-                                   showarrow=True, arrowhead=1, ax=0, ay=-40)
-            fig_age.add_annotation(x=min_age, y=min_count,
-                                   text=f'Edad: {min_age}<br>Min: {min_count}',
-                                   showarrow=True, arrowhead=1, ax=0, ay=40)
-            fig_age.add_shape(type='line', x0=df['edad'].min(), x1=df['edad'].max(), y0=mean_count, y1=mean_count,
-                              line=dict(color='RoyalBlue', dash='dash'))
-            fig_age.add_annotation(x=df['edad'].max(), y=mean_count,
-                                   text=f'Mean: {mean_count:.2f}', showarrow=False, yshift=10)
-            #fig_age.update_traces(marker_color='#00CC96', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
-            #fig_age.update_layout(plot_bgcolor='rgba(0,0,0,0)', paper_bgcolor='rgba(0,0,0,0)', font_color='#FFFFFF', title_font_color='#FF6F61', title_font_size=20)
-            st.plotly_chart(fig_age)
-
-        with col2:
-            st.subheader('Número de Personas Desaparecidas por País de Nacimiento')
-            country_counts = df['pais_nacimiento'].value_counts().reset_index()
-            country_counts.columns = ['País de Nacimiento', 'Número de Personas']
-            fig_country = px.pie(country_counts, names='País de Nacimiento', values='Número de Personas',
-                                 title='Número de Personas Desaparecidas por País de Nacimiento',
-                                 color_discrete_sequence=px.colors.qualitative.Pastel, hole=0.4)
-            fig_country.update_traces(textposition='inside', textinfo='percent+label')
-            st.plotly_chart(fig_country)
-
-        with col1:
-            st.subheader('Personas Desaparecidas por Dependencia Policial')
-            top_n = st.slider('Selecciona el número de dependencias policiales a mostrar:', min_value=1, max_value=20, value=10)
-            police_counts = df['dependencia_policial'].value_counts().reset_index()
-            police_counts.columns = ['Dependencia Policial', 'Número de Personas']
-            top_police_counts = police_counts.head(top_n)
-            fig_police = px.bar(top_police_counts, x='Dependencia Policial', y='Número de Personas',
-                                title=f'Top {top_n} Dependencias Policiales con más Personas Desaparecidas',
-                                color_discrete_sequence=['#00CC96'])
-            st.plotly_chart(fig_police)
-
-        with col2:
-            st.subheader('Personas Desaparecidas a lo Largo del Tiempo')
-            df['fecha_denuncia'] = pd.to_datetime(df['fecha_denuncia'], format='%d/%m/%Y', errors='coerce')
-            df_time = df['fecha_denuncia'].value_counts().sort_index().reset_index()
-            df_time.columns = ['Fecha de Denuncia', 'Número de Personas']
-            fig_time = px.line(df_time, x='Fecha de Denuncia', y='Número de Personas', title='Personas Desaparecidas a lo Largo del Tiempo', color_discrete_sequence=['#AB63FA'])
-            st.plotly_chart(fig_time)
-            
-    with st.sidebar:
-        st.markdown("<h2 style='color: #FF6F61;'>Filtros</h2>", unsafe_allow_html=True)
-        st.markdown("<hr style='border: 1px solid #FF6F61;'>", unsafe_allow_html=True)
-        # Filtros adicionales pueden ser añadidos aquí
 
     # Footer
     st.markdown("<hr style='border: 1px solid #FF6F61;'>", unsafe_allow_html=True)
     st.markdown("<h4 style='text-align: center; color: #FF6F61;'>© 2024 Los Saqueadores</h4>", unsafe_allow_html=True)
+
+
+if selected == "Apriori":
+    st.header('Patrones frecuentes y reglas de asociación')
+   
+
+
+if selected == "Random Forest":
+    st.header('Random Forest')
+
+
+if selected == "Clusters":
+    st.header('Grupos Asociados')
+
+
+if selected == "ARIMA":
+    st.header('Modelo Estacional de Tendencias')
