120a2d6fef8aefcf89b60c47120bc437711f5b72
JuanC-L <jc.lindom@gmail.com>
2024-06-27 13:23:07 -0500

    ayuda2

--- streamlit_app.py
+++ streamlit_app.py
@@ -387,8 +387,8 @@ if selected == "Random Forest":
 if selected == "Clusters":
     st.header('Grupos Asociados')
     cluster_data = {
-        "Cluster_0_vs_Cluster_1": [-0.002641, 0.190385, 3.573445, 0.046478, 0.025184, -0.139584, -0.034321, 0.004827, 0.036518, -0.093174],
-        "Cluster_1_vs_Cluster_0": [0.002801, -0.182160, -3.359249, -0.047260, -0.025308, 0.149832, 0.034357, -0.004926, -0.036908, 0.102972]
+        "Cluster_0_vs_Cluster_1": [-0.002641, 0.190385, 3.573445, 0.046478, 0.025184],
+        "Cluster_1_vs_Cluster_0": [0.002801, -0.182160, -3.359249, -0.047260, -0.025308]
     }
     cluster_index = ["Horas para Denunciar", "LUGAR DEL HECHO", "Instructor Policial", "EDAD", "ESTATURA", "NARIZ", "TEZ", "SANGRE", "CONTEXTURA", "FENOTIPO"]
     cluster_df = pd.DataFrame(cluster_data, index=cluster_index)
@@ -406,11 +406,35 @@ if selected == "Clusters":
         st.markdown("<h4 style='text-align: center;'>TSNE</h2>", unsafe_allow_html=True)
         st.image('images/tnse.jpeg', caption='', use_column_width=True)
 
-    col1, col2 = st.columns([5, 1])
-    with col1:
-        st.header('Perfiles')
-        st.image('images/perfiles.png', caption='')
-        
+    st.header('Perfiles de Personas Desaparecidas')
+    st.subheader('Perfil 1:')
+    st.markdown('''
+    - *Denuncia Inmediata*: Las denuncias suelen presentarse casi inmediatamente.
+    - *Lugar del Hecho Específico*: Las desapariciones tienden a ocurrir en lugares específicos o identificables.
+    - *Relación con Instructor Policial*: Alta probabilidad de tener algún tipo de relación con un instructor policial.
+    - *Edad*: Ligeramente mayores que el promedio.
+    - *Estatura*: Ligeramente más altos que el promedio.
+    - *Nariz Prominente*: Forma de la nariz más prominente.
+    - *Tez Ligeramente Más Oscura*: Tez con una ligera variación negativa respecto al promedio.
+    - *Tipo de Sangre*: Muestra una mínima variación positiva respecto al promedio.
+    - *Contextura Robusta*: Ligeramente más robustos que el promedio.
+    - *Fenotipo Variado*: Características físicas con variaciones negativas respecto al promedio.
+    ''')
+
+    st.subheader('Perfil 2:')
+    st.markdown('''
+    - *Denuncia Levemente Demorada*: Las denuncias tienden a presentarse con una mínima variación positiva en el tiempo.
+    - *Lugar del Hecho Menos Específico*: Las desapariciones tienden a ocurrir en lugares menos específicos.
+    - *Sin Relación con Instructor Policial*: Menos probabilidad de tener relación con un instructor policial.
+    - *Edad*: Ligeramente menores que el promedio.
+    - *Estatura*: Ligeramente más bajos que el promedio.
+    - *Nariz Menos Prominente*: Forma de la nariz menos prominente.
+    - *Tez Ligeramente Más Clara*: Tez con una ligera variación positiva respecto al promedio.
+    - *Tipo de Sangre*: Muestra una mínima variación negativa respecto al promedio.
+    - *Contextura Menos Robusta*: Ligeramente menos robustos que el promedio.
+    - *Fenotipo Consistente*: Características físicas con variaciones positivas respecto al promedio.
+    ''')
+
 if selected == "ARIMA":
     st.header('Modelo Estacional de Tendencias')
     col1, col2 = st.columns([5, 1])
